0d096c92a70deecdabebddc4afa19e9561ea11f5
Kerem Kuyucu <84400545+KeremKuyucu@users.noreply.github.com>
2026-01-18 15:27:28 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
-export default function HomePage() {
-  return null
+// app/page.tsx
+import { createClient } from "@/lib/supabase/client"
+import { redirect } from "next/navigation"
+
+export default async function HomePage() {
+  const supabase = createClient()
+  const { data } = await supabase.auth.getUser()
+
+  if (data.user) {
+    redirect("/dashboard") // loginli kullanıcı dashboard’a
+  } else {
+    redirect("/login")     // loginli değilse login sayfasına
+  }
 }
